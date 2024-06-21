5fcec4389b0153d6d193163d39a2479821d8b485
Mateusz Różański <rozanski.mateusz00@gmail.com>
2024-06-21 04:09:07 +0200

    test: write tests for ToOk success state cases with mapping to contract objects

--- test/ErrorOrAspNetCoreExtensions.Tests.Unit/ErrorOrAspNetCoreExtensions.ToOkTests.cs
+++ test/ErrorOrAspNetCoreExtensions.Tests.Unit/ErrorOrAspNetCoreExtensions.ToOkTests.cs
@@ -49,6 +49,65 @@ public class ToOkTests
             .Be(StatusCodes.Status400BadRequest);
     }
 
+    [Fact]
+    public void ToOk_ShouldReturnOkHttpResult_WhenResultIsSuccess()
+    {
+        // Arrange
+        ErrorOr<Success> errorOrSuccess = new Success();
+
+        // Act
+
+        var result = errorOrSuccess.ToOk();
+
+        // Assert
+
+        result
+            .Should()
+            .BeOfType<Ok<Success>>()
+            .Which.StatusCode.Should()
+            .Be(StatusCodes.Status200OK);
+    }
+
+    [Fact]
+    public void ToOk_ShouldReturnOkHttpResult_AndMapToContract_WhenResultIsSuccess()
+    {
+        // Arrange
+
+        var testGuid = "4f048803-2f4b-444e-8570-95bb4d80d887";
+
+        var id = Guid.Parse(testGuid);
+        var firstName = "John";
+        var lastName = "Smith";
+
+        var expectedId = testGuid;
+        var expectedName = $"{firstName} {lastName}";
+
+        var expectedApiContractResponse = new UserApiResponse(expectedId, expectedName);
+
+        var mapper = (User user) =>
+            new UserApiResponse(user.Id.ToString(), $"{user.FirstName} {user.LastName}");
+
+        ErrorOr<User> errorOr = new User(id, firstName, lastName);
+
+        // Act
+
+        var result = errorOr.ToOk(mapper);
+
+        // Assert
+
+        result
+            .Should()
+            .BeOfType<Ok<UserApiResponse>>()
+            .Which.Should()
+            .Match<Ok<UserApiResponse>>(
+                r =>
+                    r.StatusCode == StatusCodes.Status200OK
+                    && r.Value != null
+                    && r.Value.Equals(expectedApiContractResponse),
+                "because status code should be 200 OK, and the success value should be mapped to the response contract"
+            );
+    }
+
     public static IEnumerable<object[]> ToOk_ShouldReturnProblemDetailsHttpResult_WhenResultIsError_Data() =>
         new[]
         {
@@ -58,4 +117,8 @@ public class ToOkTests
             [Error.Unauthorized(), StatusCodes.Status401Unauthorized],
             [Error.Failure(), StatusCodes.Status500InternalServerError],
         };
+
+    private record User(Guid Id, string FirstName, string LastName);
+
+    private record UserApiResponse(string Id, string Name);
 }
