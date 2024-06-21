abeb38c1b0f38788b268b60595781af40606ddc1
Mateusz Różański <rozanski.mateusz00@gmail.com>
2024-06-21 12:16:04 +0200

    feat: add string uri overloads in ToCreated method

--- src/ErrorOrAspNetCoreExtensions/ErrorOrAspNetCoreExtensions.Created.cs
+++ src/ErrorOrAspNetCoreExtensions/ErrorOrAspNetCoreExtensions.Created.cs
@@ -5,12 +5,27 @@ namespace ErrorOrAspNetCoreExtensions;
 
 public static partial class ErrorOrAspNetCoreExtensions
 {
+    public static IResult ToCreatedWithoutBody(this IErrorOr result, Uri createdAtUri) =>
+        result.IsError ? result.Errors!.ToProblem() : TypedResults.Created(createdAtUri);
+
+    public static IResult ToCreatedWithoutBody(this IErrorOr result, string createdAtUri) =>
+        result.IsError ? result.Errors!.ToProblem() : TypedResults.Created(createdAtUri);
+
     public static IResult ToCreated<TResult>(this ErrorOr<TResult> result, Uri createdAtUri) =>
+        result.Match(value => TypedResults.Created(createdAtUri, value), ToProblem);
+
+    public static IResult ToCreated<TResult>(this ErrorOr<TResult> result, string createdAtUri) =>
         result.Match(value => TypedResults.Created(createdAtUri, value), ToProblem);
 
     public static IResult ToCreated<TResult, TContract>(
         this ErrorOr<TResult> result,
         Uri createdAtUri,
+        Func<TResult, TContract> mapper
+    ) => result.Match(value => TypedResults.Created(createdAtUri, mapper(value)), ToProblem);
+
+    public static IResult ToCreated<TResult, TContract>(
+        this ErrorOr<TResult> result,
+        string createdAtUri,
         Func<TResult, TContract> mapper
     ) => result.Match(value => TypedResults.Created(createdAtUri, mapper(value)), ToProblem);
 }
